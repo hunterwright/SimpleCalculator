cfc9354ea8c6cb5dfb838ece45429d5e1edc7d85
hunterwright <wright.hunter22@gmail.com>
2017-02-22 13:43:48 -0600

    Finished bounds, started functions

--- src/CalculatorFrame.java
+++ src/CalculatorFrame.java
@@ -1,5 +1,4 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 /**
  * Frame of the calculator.
@@ -8,14 +7,31 @@ public class CalculatorFrame extends JFrame {
     JButton[] numBtn = new JButton[10];
     JButton[] opBtn = new JButton[8];
 
+    JTextField txt_input = new JTextField("");
+    JTextField txt_output = new JTextField("");
+
+    private String s = "";
+
+    private boolean isMult = false;
+    private boolean isDiv = false;
+    private boolean isAdd = false;
+    private boolean isSub = false;
+
     public CalculatorFrame() {
         // FRAME SETUP -----------------------------------------
         super("Simple Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(330, 400);
+        setSize(335, 400);
         setLayout(null);
         setVisible(true);
+        setResizable(false);
         //------------------------------------------------------
+
+        txt_input.setBounds(5,5,320,20);
+        add(txt_input);
+        txt_input.setHorizontalAlignment(JLabel.RIGHT);
+        txt_output.setBounds(5,35,320,20);
+        add(txt_output);
 
         // SETS NUMBER BUTTONS
         for (int i = 0; i < numBtn.length; i++) {
@@ -47,22 +63,97 @@ public class CalculatorFrame extends JFrame {
         opBtn[6] = new JButton("/");
         opBtn[7] = new JButton("=");
         // SET OPERATOR BUTTON BOUNDS
-        opBtn[0].setBounds(200, 135, 60, 190);
-        opBtn[1].setBounds(5, 135, 60, 60);
-        opBtn[2].setBounds(70, 135, 60, 60);
-        opBtn[3].setBounds(135, 135, 60, 60);
-        opBtn[4].setBounds(5, 200, 60, 60);
-        opBtn[5].setBounds(70, 200, 60, 60);
-        opBtn[6].setBounds(135, 200, 60, 60);
-        opBtn[7].setBounds(5, 265, 60, 60);
+        opBtn[0].setBounds(70, 70, 60, 60);
+        opBtn[1].setBounds(135, 70, 60, 60);
+        opBtn[2].setBounds(200, 70, 60, 60);
+        opBtn[3].setBounds(265, 70, 60, 60);
+        opBtn[4].setBounds(265, 135, 60, 60);
+        opBtn[5].setBounds(265, 200, 60, 60);
+        opBtn[6].setBounds(265, 265, 60, 60);
+        opBtn[7].setBounds(5, 335, 320, 35);
         // ADDS BUTTONS
         for (int i = 0; i < opBtn.length; i++) {
             add(opBtn[i]);
         }
 
-        // BUTTON FUNCTIONS
+        // NUMBER BUTTON FUNCTIONS
         numBtn[0].addActionListener(e -> {
+            s+="0";
+            txt_input.setText(s);
+        });
+        numBtn[1].addActionListener(e -> {
+            s+="1";
+            txt_input.setText(s);
+        });
+        numBtn[2].addActionListener(e -> {
+            s+="2";
+            txt_input.setText(s);
+        });
+        numBtn[3].addActionListener(e -> {
+            s+="3";
+            txt_input.setText(s);
+        });
+        numBtn[4].addActionListener(e -> {
+            s+="4";
+            txt_input.setText(s);
+        });
+        numBtn[5].addActionListener(e -> {
+            s+="5";
+            txt_input.setText(s);
+        });
+        numBtn[6].addActionListener(e -> {
+            s+="6";
+            txt_input.setText(s);
+        });
+        numBtn[7].addActionListener(e -> {
+            s+="7";
+            txt_input.setText(s);
+        });
+        numBtn[8].addActionListener(e -> {
+            s+="8";
+            txt_input.setText(s);
+        });
+        numBtn[9].addActionListener(e -> {
+            s+="9";
+            txt_input.setText(s);
+        });
 
+
+        // OPERATOR BUTTON FUNCTIONS
+        opBtn[0].addActionListener(e -> {
+            String str = txt_input.getText();
+            if(!str.contains(".")) {
+                s+=".";
+                txt_input.setText(s);
+            }
+        });
+        opBtn[1].addActionListener(e -> {
+            s+="";
+            txt_input.setText(s);
+        });
+        opBtn[2].addActionListener(e -> {
+            txt_input.setText("");
+            s = "";
+        });
+        opBtn[3].addActionListener(e -> {
+            s+="+";
+            txt_input.setText(s);
+        });
+        opBtn[4].addActionListener(e -> {
+            s+="-";
+            txt_input.setText(s);
+        });
+        opBtn[5].addActionListener(e -> {
+            s+="*";
+            txt_input.setText(s);
+        });
+        opBtn[6].addActionListener(e -> {
+            s+="/";
+            txt_input.setText(s);
+        });
+        opBtn[7].addActionListener(e -> {
+            txt_input.setText("");
+            s = "";
         });
 
     }
